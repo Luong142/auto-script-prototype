e551d02ac398366b40e20f8409fec78943806a67
Liang Luong <66522372+Luong142@users.noreply.github.com>
2024-08-03 13:26:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# auto-script-prototype
+ this is to test pyautogui for security purpose
---
+++ testing.py
@@ -0,0 +1,10 @@
+# day la comment, comment la su dung de ghi chu
+
+i = 10
+name = "Ten la gi"
+x = 20
+y = 20.5
+
+b = x + y
+
+print()
